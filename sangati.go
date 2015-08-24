17acc3db98f2fcd62be5f8d21c84a37e1e8b0716
Farhan Ahmed <farhan.ahmed@axial.net>
2015-08-24 15:18:54 -0400

    Basic JSON deserialization

--- sangati.go
+++ sangati.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"os"
+	"fmt"
+)
+
+type Test struct {
+	Queries	[]string
+	Count	interface{}
+}
+
+type Configuration struct {
+	Tests	[]Test
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	file, err := os.Open("src/github.com/alienfluid/sangati/test.json")
+	if err != nil {
+		fmt.Println("error reading file:", err)
+	}
+
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Println(configuration.Tests)
 }
